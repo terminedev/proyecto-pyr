fbcf5a38f781a2371d3645ff8be37341cfefb0c7
terminedev <terminegastondev@gmail.com>
2026-07-10 15:18:10 -0300

    GameInProgress estilizado

--- src/interfaz/GameInProgress/GameInProgress.tsx
+++ src/interfaz/GameInProgress/GameInProgress.tsx
@@ -21,13 +21,13 @@ export default function GameInProgress({
         questions
     } = gameData;
 
-    return <section className="center-web-page center-all" style={{ gap: '1rem' }}>
-        <p style={{ textAlign: 'center' }}>¡Puntuación actual: <strong>{scoreInGame}</strong>pts!</p>
-        <ul className="center-list ">
-            <li>Ahora: {gameHistory?.currentQuestion}</li>
-            <li>Totales: {gameHistory?.totalQuestions}</li>
-            <li style={{ color: 'green' }}>Correctas: {gameHistory?.correctQuestions}</li>
-            <li style={{ color: 'red' }}>Erradas: {gameHistory?.wrongQuestions}</li>
+    return <section className="center-web-page center-all" style={{ gap: '2rem' }}>
+        <p className="score" style={{ textAlign: 'center' }}>¡Puntuación actual: <strong className="score-point">{scoreInGame}</strong>pts!</p>
+        <ul className="center-list">
+            <li className="score" style={{ filter: 'drop-shadow(0 0 3px #4f4f4f)' }}>Ahora: {gameHistory?.currentQuestion}</li>
+            <li className="score" style={{ filter: 'drop-shadow(0 0 3px #4f4f4f)' }}>Totales: {gameHistory?.totalQuestions}</li>
+            <li className="score" style={{ color: '#e3f7e3', filter: 'drop-shadow(0 0 3px #026406)' }}>Correctas: {gameHistory?.correctQuestions}</li>
+            <li className="score" style={{ color: '#fa98a5', filter: 'drop-shadow(0 0 3px #940417)' }}>Erradas: {gameHistory?.wrongQuestions}</li>
         </ul>
 
         <QuizList
